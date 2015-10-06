6c99763f3a9fa74b6d3b5b0c2c117e042a705fce
Gary Anderson <git@garyvictoranderson.com>
2015-10-06 08:56:38 -0700

    add compass reading

--- www/js/app.js
+++ www/js/app.js
@@ -2,14 +2,23 @@ var deviceReadyDeferred = $.Deferred();
 var jqmReadyDeferred = $.Deferred();
 
 $(document).on("deviceready", function() {
-  deviceReadyDeferred.resolve();
+    deviceReadyDeferred.resolve();
 });
 
-$(document).on("mobileinit", function () {
-  jqmReadyDeferred.resolve();
+$(document).on("mobileinit", function() {
+    jqmReadyDeferred.resolve();
 });
 
 $.when(deviceReadyDeferred, jqmReadyDeferred).then(init);
 
 function init() {
+    navigator.compass.getCurrentHeading(success, failure);
+}
+
+function success(heading) {
+    document.getElementById('heading').innerHTML = heading.magneticHeading;
+}
+
+function failure(error) {
+    alert("Compass Error: " + error.code)
 }
